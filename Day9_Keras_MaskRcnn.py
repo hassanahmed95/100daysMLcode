e78ea77b62c70714c9676d0662ca2a101c91c92c
hassanahmed95 <mr.hassanahmed95@gmail.com>
2020-03-06 17:32:15 +0500

    the mask RCNN for image detection in the image . . .

---
+++ Day9_Keras_MaskRcnn.py
@@ -0,0 +1,19 @@
+from mrcnn.config import Config
+from mrcnn import model as modellib
+from mrcnn import visualize
+import numpy as np
+import colorsys
+import argparse
+import imutils
+import random
+import cv2
+import os
+
+# first of all the required package for maskrcnn which is mrcnn
+# mrcnn has been installed using official repo of the mask rcnn as following
+# $ git clone https://github.com/matterport/Mask_RCNN.git
+# $ cd Mask_RCNN
+# $ python setup.py install
+
+
+print("hello there I am going to perform object detection using maskRcnn")
